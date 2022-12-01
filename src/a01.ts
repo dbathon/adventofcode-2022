23742c4420f5bf9f6569200da87b0b245a7ee116
Dominik Bathon <dominik.bathon@gmail.com>
2022-12-01 08:22:12 +0100

    Solve day 1

---
+++ src/a01.ts
@@ -0,0 +1,18 @@
+import { findMax, p, readLines, sum } from "./util/util";
+
+const lines = readLines("input/a01.txt", true, false);
+
+const caloriesPerElf: number[][] = [[]];
+
+for (const line of lines) {
+  if (line.length > 0) {
+    caloriesPerElf[caloriesPerElf.length - 1].push(parseInt(line));
+  } else {
+    caloriesPerElf.push([]);
+  }
+}
+
+const sums = caloriesPerElf.map((calories) => sum(calories));
+sums.sort((a, b) => b - a);
+p(sums[0]);
+p(sums[0] + sums[1] + sums[2]);
