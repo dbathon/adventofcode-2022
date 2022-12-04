d6a368e4319d373e5d54dd0a5a5f7a80738833f5
Dominik Bathon <dominik.bathon@gmail.com>
2022-12-04 13:48:59 +0100

    Solve day 4

--- src/a04.ts
+++ src/a04.ts
@@ -1,5 +1,28 @@
 import { p, readLines } from "./util/util";
 
-const lines = readLines("input/a04.txt");
+class Range {
+  constructor(readonly from: number, readonly to: number) {}
 
-p(lines);
+  contains(value: number) {
+    return this.from <= value && value <= this.to;
+  }
+
+  containsCompletely(other: Range) {
+    return this.contains(other.from) && this.contains(other.to);
+  }
+
+  overlaps(other: Range) {
+    return this.contains(other.from) || this.contains(other.to) || other.contains(this.from);
+  }
+}
+
+const rangePairs = readLines("input/a04.txt").map((line) =>
+  line.split(",").map((r) => {
+    const [from, to] = r.split("-");
+    return new Range(parseInt(from), parseInt(to));
+  })
+);
+
+p(rangePairs.filter((pair) => pair[0].containsCompletely(pair[1]) || pair[1].containsCompletely(pair[0])).length);
+
+p(rangePairs.filter((pair) => pair[0].overlaps(pair[1])).length);
