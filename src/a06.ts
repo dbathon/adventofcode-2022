9cd54b67c2f58b4d034a8d24b2353f4e9bd2a3c5
Dominik Bathon <dominik.bathon@gmail.com>
2022-12-06 10:18:38 +0100

    Solve day 6

--- src/a06.ts
+++ src/a06.ts
@@ -1,5 +1,20 @@
 import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a06.txt");
+const line = lines[0];
 
-p(lines);
+function lettersUnqiue(str: string) {
+  const set = new Set(str.split(""));
+  return set.size === str.length;
+}
+
+function findMarkerEnd(str: string, length: number) {
+  for (let i = 0; i < line.length - (length - 1); i++) {
+    if (lettersUnqiue(line.substring(i, i + length))) {
+      return i + length;
+    }
+  }
+}
+
+p(findMarkerEnd(line, 4));
+p(findMarkerEnd(line, 14));
