6b42cac611056b7bad6e1268b19b634ed1d12455
Dominik Bathon <dominik.bathon@gmail.com>
2022-12-13 00:22:30 +0100

    Optimize day 12
    Solve part 2 by searching once from E.

--- src/a12.ts
+++ src/a12.ts
@@ -1,17 +1,13 @@
 import { dijkstraSearch, Neighbor } from "./util/graphUtil";
 import { Map2D, Map2DNode } from "./util/map2D";
-import { findMax, p, readLines } from "./util/util";
+import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a12.txt");
 
 const map = new Map2D<string>();
-let start: Map2DNode<string> = map.getNode(0, 0);
 lines.forEach((line, y) => {
   line.split("").forEach((letter, x) => {
     map.set(x, y, letter);
-    if (letter === "S") {
-      start = map.getNode(x, y);
-    }
   });
 });
 
@@ -19,16 +15,22 @@ function height(letter: string) {
   return letter === "S" ? "a" : letter === "E" ? "z" : letter;
 }
 
-function pathToELength(start: Map2DNode<string>): number | undefined {
+type Node = Map2DNode<string>;
+
+function pathLength(
+  start: Node,
+  goalPredicate: (node: Node) => unknown,
+  neighborPredicate: (node: Node, neighbor: Node) => unknown
+): number | undefined {
   let result: number | undefined = undefined;
-  dijkstraSearch<Map2DNode<string>, void>(
+  dijkstraSearch<Node, void>(
     (node, _, distance) => {
-      if (!node.value || node.value === "E") {
+      if (goalPredicate(node)) {
         result = distance;
         return null;
       }
       return node.fourNeighbors
-        .filter((n) => n.value && height(n.value).charCodeAt(0) - height(node.value!).charCodeAt(0) <= 1)
+        .filter((n) => neighborPredicate(node, n))
         .map((n) => {
           return new Neighbor(n, 1, undefined);
         });
@@ -39,12 +41,20 @@ function pathToELength(start: Map2DNode<string>): number | undefined {
   return result;
 }
 
-p(pathToELength(start));
+p(
+  pathLength(
+    map.getNodes().find((node) => node.value === "S")!,
+    (node) => node.value === "E",
+    (node, neighbor) =>
+      node.value && neighbor.value && height(neighbor.value).charCodeAt(0) - height(node.value).charCodeAt(0) <= 1
+  )
+);
 
-const pathLengths = map
-  .getNodes()
-  .filter((node) => node.value && height(node.value) === "a")
-  .map((node) => pathToELength(node))
-  .filter((node) => !!node) as number[];
-
-p(pathLengths.sort((a, b) => a - b)[0]);
+p(
+  pathLength(
+    map.getNodes().find((node) => node.value === "E")!,
+    (node) => node.value && height(node.value) === "a",
+    (node, neighbor) =>
+      node.value && neighbor.value && height(neighbor.value).charCodeAt(0) - height(node.value).charCodeAt(0) >= -1
+  )
+);
